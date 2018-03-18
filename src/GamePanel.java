6d9a84079ea6bb407b056a56777e1084b681de80
Daniel Binoy <dbinoy15@gmail.com>
2018-03-18 08:57:48 -0700

    variable scale
    the distance to the mouse now determines the length of each vector. Further = longer

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -15,6 +15,10 @@ public  class GamePanel extends JPanel{
 		@Override
 		public void paintComponent(Graphics g){
 			Graphics2D g2d = (Graphics2D) g;
+			PointerInfo a = MouseInfo.getPointerInfo();
+      Point b = a.getLocation();
+      int mouseX = (int) (b.getX());
+      int mouseY = (int) (b.getY());
 			//clear
 			g2d.setColor(Color.BLACK);
 			g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
@@ -22,6 +26,8 @@ public  class GamePanel extends JPanel{
 			//render each vector
 			for(int i = 0; i < handler.getVectors().size(); i++){
 				Vector2 vector = handler.getVectors().get(i);
+				scale = vector.dist(new Vector2(mouseX,mouseY),vector);
+				scale/=10;
 				g2d.setColor(Color.RED);
 				int x2 = (int)(scale*Math.cos(Math.toRadians(vector.getAngle()))+vector.getX());
 				int y2 = (int)(scale*Math.sin(Math.toRadians(vector.getAngle()))+vector.getY());
